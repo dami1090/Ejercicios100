dfd2f796918962e1de7806f79037dd02102c4a00
dami! <dami!@DESKTOP-DG45CII>
2021-03-21 00:04:15 -0300

    Ejercicio15

---
+++ Ejercicio/ClassLibrary1/Calculadora.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDami
+{
+    public class Calculadora
+    {
+        public static double Calcular(double n1, double n2, char operador)
+        {
+            double resultado;
+            switch (operador)
+            {
+                case '+':
+                    resultado = n1 + n2;
+                    break;
+                case '-':
+                    resultado = n1 - n2;
+                    break;
+                case '*':
+                    resultado = n1 * n2;
+                    break;
+                case '/':
+                    if(Validar(n2))
+                    {
+                        resultado = n1 / n2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No se puede dividir por 0 o por negativos");
+                        resultado = 0;
+                    }
+                    break;
+                default:
+                    resultado = -1;
+                    break;
+
+            }          
+            return resultado;
+        }
+
+        private static bool Validar(double divisor)
+        {
+            bool respuesta = false;
+            if (divisor > 0 )
+            {
+                respuesta = true;
+            }
+            return respuesta;
+        }
+    }
+}
---
+++ Ejercicio/Ejercicio15/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaDami;
+
+
+namespace Ejercicio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char respuesta;
+            int num,num2;
+            string preg;
+            char simbolo;
+            bool continuar = true;
+            double result;
+
+            while (continuar)
+            {
+                continuar = false;
+                Console.WriteLine("Caculadora");
+
+                do
+                {
+                    Console.Write("Ingrese el valor del primer numero: ");
+                    preg = Console.ReadLine();
+
+                } while (!int.TryParse(preg, out num));
+                do
+                {
+                    Console.Write("Ingrese el valor del segundo numero: ");
+                    preg = Console.ReadLine();
+
+                } while (!int.TryParse(preg, out num2));
+
+                Console.WriteLine("ahora debera seleccionar que operacion realizar: ");
+                do
+                {
+                    Console.Write("Suma(+) Resta(-) Multipilicacion(*)  Division(/): ");
+                    simbolo = Console.ReadKey().KeyChar;
+                    Console.WriteLine();
+                } while (simbolo != '+' && simbolo != '-' && simbolo != '*' && simbolo != '/');
+                result = Calculadora.Calcular(num, num2, simbolo);
+                Console.WriteLine("El resultado es: {0}", result);
+
+
+                Console.WriteLine("¿Continuar ? (S / N) ");
+                Console.Write("Respuesta: ");
+                respuesta = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                if (Validacion.ValidarS_N(respuesta))
+                {
+                    continuar = true;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
