550b8c8d4d39ef7d2fa55d44f957ce05c1c03255
dami! <dami!@DESKTOP-DG45CII>
2021-03-21 02:06:56 -0300

    Ejercicio 17

---
+++ Ejercicio/ClassLibrary1/Boligrafo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDami
+{
+    public class Boligrafo
+    {
+        public short cantidadTintaMaxima = 100;
+        ConsoleColor color;
+        short tinta;
+
+        public Boligrafo(short tinta, ConsoleColor color)
+        {
+            this.tinta = tinta;
+            this.color = color;
+            Console.WriteLine("Tinta: {0},Color{1}", this.tinta, this.color);
+        }
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+        public bool Pintar(short gasto, out string dibujo)
+        {
+            bool flag = false;
+            string concatenar = "";
+            int dif;
+            int cantidadTinta = this.tinta;
+            dif = this.tinta - gasto;
+            if (dif >= 0)
+            {
+                SetTinta((short)(gasto*(-1)));
+
+                    while (gasto > 0)
+                    {
+                        concatenar += "*";
+                        gasto--;
+                    }
+                    flag = true;
+            }
+            else
+            {
+                while (cantidadTinta > 0)
+                {
+                    concatenar += "*";
+                    cantidadTinta--;
+                }
+                this.tinta = 0;
+            }
+
+            dibujo = concatenar;
+            return flag;
+        }
+        public void Recargar()
+        {
+            this.tinta = 100;
+        }
+        private void SetTinta(short tinta)
+        {
+            if (tinta > 0)
+            {
+                if(this.tinta + tinta >= 100)
+                {
+                    this.Recargar();
+                }
+                this.tinta += tinta;
+            }
+            else
+            {
+                this.tinta += tinta;
+            }
+
+            //if (this.tinta >= 100)
+            //{
+            //    Recargar();
+            //}
+            //if (this.tinta <= 0)
+            //{
+            //    this.tinta = 0;
+            //}
+            return;
+        }
+
+    }
+}
---
+++ Ejercicio/Ejercicio17/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaDami;
+
+namespace Ejercicio17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short cantidadRed = 50;
+            short cantidadBlue = 100;
+            string draw;
+            //ConsoleColor color = ConsoleColor.Red;
+            Boligrafo lapiceraRed = new Boligrafo(cantidadRed, ConsoleColor.Red);
+            Boligrafo lapiceraBlue = new Boligrafo(cantidadBlue, ConsoleColor.Blue);
+
+            if (lapiceraRed.Pintar(55, out draw))
+            {
+                Console.ForegroundColor = lapiceraRed.GetColor();
+                Console.WriteLine("Pudo acabar el dibujo");
+                Console.WriteLine("resultado: {0}", draw);
+            }
+            else
+            {
+                Console.ForegroundColor = lapiceraRed.GetColor();
+                Console.WriteLine("el dibujo no se completo, no habia tinta suficiente");
+                Console.WriteLine("draw: {0}", draw);
+            }
+
+            if (lapiceraBlue.Pintar(30, out draw))
+            {
+                Console.ForegroundColor = lapiceraBlue.GetColor();
+                Console.WriteLine("Pudo acabar el dibujo");
+                Console.WriteLine("resultado: {0}", draw);
+            }
+            else
+            {
+                Console.ForegroundColor = lapiceraBlue.GetColor();
+                Console.WriteLine("el dibujo no se completo, no habia tinta suficiente");
+                Console.WriteLine("draw: {0}", draw);
+            }
+
+            lapiceraRed.Recargar();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Recargamos la lapicera ROJA");
+            if (lapiceraRed.Pintar(55, out draw))
+            {
+                Console.ForegroundColor = lapiceraRed.GetColor();
+                Console.WriteLine("Pudo acabar el dibujo");
+                Console.WriteLine("resultado: {0}", draw);
+            }
+            else
+            {
+                Console.ForegroundColor = lapiceraRed.GetColor();
+                Console.WriteLine("No se termino la tinta y pudo acabar el dibujo");
+                Console.WriteLine("draw: {0}", draw);
+            }
+
+            Console.ForegroundColor = lapiceraBlue.GetColor();
+            Console.WriteLine("Lapicera azul cantidad de tinta: {0}", lapiceraBlue.GetTinta());
+            Console.ForegroundColor = lapiceraRed.GetColor();
+            Console.WriteLine("Lapicera roja cantidad de tinta: {0}", lapiceraRed.GetTinta());
+            Console.ReadLine();
+        }
+    }
+}
