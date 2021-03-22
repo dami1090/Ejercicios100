71d01b531abc810443f4c1ba31ccd0850a2d0d6a
dami! <dami!@DESKTOP-DG45CII>
2021-03-21 23:45:46 -0300

    Ejercicio 16

---
+++ Ejercicio/ClassLibrary1/Alumno.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDami
+{
+    public class Alumno
+    {
+        byte nota1;
+        byte nota2;
+        float notaFinal;
+        public string apellido;
+        public int legajo;
+        public string nombre;
+
+        public Alumno(string nom, string ape, int leg)
+        {
+            this.nombre = nom;
+            this.apellido = ape;
+            this.legajo = leg;
+        }
+        public void Mostrar()
+        {
+            Console.WriteLine("Nombre: {0}, Apellido: {1}, Nro legajo: {2}, Nota 1: {3}, Nota 2: {4}", this.nombre, this.apellido, this.legajo, this.nota1, this.nota2);
+            if (this.notaFinal != -1)
+            {
+                Console.WriteLine("Nota Final: {0}", this.notaFinal);
+            }
+            else
+            {
+                Console.WriteLine("Alumno desaprobado");
+            }
+        }
+        public void Estudiar(byte n1, byte n2)
+        {
+            this.nota1 = n1;
+            this.nota2 = n2;
+        }
+        public void CalcularFinal()
+        {
+            if(this.nota1>=4 && this.nota1 <=10 && this.nota2 >= 4 && this.nota2 <= 10)
+            {
+                this.notaFinal = (float)new Random().Next(4, 10);
+                Console.WriteLine(this.notaFinal);
+            }
+            else
+            {
+                this.notaFinal = -1;
+                Console.WriteLine(this.notaFinal);
+            }
+        }
+    }
+}
---
+++ Ejercicio/Ejercicio16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaDami;
+
+namespace Ejercicio16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alum1 = new Alumno("Juanpi", "Torrico", 76);
+            Alumno alum2 = new Alumno("Gloria", "ibino", 42);
+            Alumno alum3 = new Alumno("Aquiles", "Baeza", 98);
+
+            alum1.Estudiar((byte)7, (byte)8);
+            alum1.CalcularFinal();
+            alum1.Mostrar();
+            Console.WriteLine();
+
+            alum2.Estudiar((byte)2, (byte)2);
+            alum2.CalcularFinal();
+            alum2.Mostrar();
+            Console.WriteLine();
+
+            alum3.Estudiar((byte)5, (byte)6);
+            alum3.CalcularFinal();
+            alum3.Mostrar();
+
+            Console.ReadLine();
+        }
+    }
+}
