460482d1212365bc76696ba3bc89350a41bfa0d9
dami! <dami!@DESKTOP-DG45CII>
2021-03-22 00:00:57 -0300

    Ejercicio 2 completo

--- Ejercicio/Ejercicio2/Program.cs
+++ Ejercicio/Ejercicio2/Program.cs
@@ -15,14 +15,13 @@ namespace Ejercicio2
             int num;
             Console.WriteLine("Ingrese un numero: ");
             preg = Console.ReadLine();
-            while (!int.TryParse(preg, out num))
+            while (!int.TryParse(preg, out num) || num < 0)
             {
                 Console.WriteLine("ERROR. ¡Reingresar número!");
                 preg = Console.ReadLine();
             }
-            Console.WriteLine("Resultado: ");
+            Console.WriteLine("Resultado al cuadrado es: {0}\nResultado al cubo es: {1}", Math.Pow(num, 2), Math.Pow(num, 3));
             Console.ReadLine();
-
         }
     }
 }
