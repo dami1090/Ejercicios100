7ffce2ce04551a0e6ef9fcbeec55750d21c11702
dami! <dami!@DESKTOP-DG45CII>
2021-03-28 23:51:48 -0300

    Ejercicio 19 por l amitad

---
+++ Ejercicio/ClassLibrary1/Sumador.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDami
+{
+    public class Sumador
+    {
+        private int cantidadSumas;
+        public int GetCantidadSumas()
+        {
+            return this.cantidadSumas;
+        }
+        public Sumador(int num)
+        {
+            this.cantidadSumas = num;
+        }
+        public Sumador()
+        {
+            this.cantidadSumas = 0;
+        }
+        public long Sumar(long a, long b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+        public string Sumar(string a, string b)
+        {
+            this.cantidadSumas++;
+            return string.Format(a + b);
+        }
+        
+    }
+}
---
+++ Ejercicio/Ejercicio19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaDami;
+
+namespace Ejercicio19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BibliotecaDami.Sumador acu = new BibliotecaDami.Sumador(21);
+
+            Console.WriteLine("Resultado de la suma de a+b: {0}",acu.Sumar("11", "58"));
+            Console.WriteLine("Cantidad de sumas: {0}",acu.GetCantidadSumas());
+            Console.ReadLine();
+        }
+    }
+}
